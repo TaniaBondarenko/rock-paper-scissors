939c334c1f693e6b8762f57f368eedaac421fef9
Tetiana Bondarenko <tn.a.bondarenko@gmail.com>
2021-06-07 21:12:36 +0300

    Updates

--- src/js/checkResults.js
+++
@@ -1,6 +0,0 @@
-import { userScore, itemScore, resultsContent } from './app';
-export default function checkResult() {
-    userScore > itemScore
-        ? resultsContent.innerHTML = `You won ${userScore}:${itemScore}`
-        : resultsContent.innerHTML = `Computer won ${itemScore}:${userScore}`;
-}
